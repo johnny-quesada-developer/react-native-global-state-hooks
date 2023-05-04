66dbf723a9b8ab40361c15e41636a68c5a8b9471
Johnny Quesada <johnny-qc@hotmail.com>
2023-05-04 06:20:15 +0200

    adding missed files

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 export * from "json-storage-formatter";
+
 export * from "./GlobalStore.types";
 export * from "./GlobalStore";
 export * from "./GlobalStoreAbstract";
 export * from "./GlobalStore.functionHooks";
+export * from "./GlobalStore.utils";
+export * from "./GlobalStore.combiners";
